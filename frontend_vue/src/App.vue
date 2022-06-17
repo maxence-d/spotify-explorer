f5d0e035a120f56094828bb0dde67f74ff040de5
LAPTOP-P21PK9CF\delat <delattre.maxence@gmail.com>
2022-06-17 23:15:50 +0900

    feat: basic navbar

--- frontend_vue/src/App.vue
+++ frontend_vue/src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Spotify-Explorer</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+        </div>
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link to="/log-in" class="button is-light">Log-in</router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <section class="section">
+      <router-view/>
+    </section>
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2022 Maxence Delattre</p>
+    </footer>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma';
 </style>
