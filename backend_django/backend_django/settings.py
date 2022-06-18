0cb003a8902cbd19bda92fcdb5ca9e37b305272f
LAPTOP-P21PK9CF\delat <delattre.maxence@gmail.com>
2022-06-18 19:34:42 +0900

    setup app model and migrations

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'djoser'
+    'spotify_explorer_app',
+    
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
